d80d95e516e2929e0c9726edc28971ace6f9423e
pdmuds4 <ryosuke3191@gmail.com>
2024-09-23 19:08:40 +0900

    tailwind導入、ディレクトリセットアップ

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,25 @@
+import { fileURLToPath } from 'url'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+  ],
+  alias: {
+    '@components': fileURLToPath(new URL('./components', import.meta.url)),
+    '@utils'     : fileURLToPath(new URL('./utils',      import.meta.url)),
+    '@models'    : fileURLToPath(new URL('./models',     import.meta.url)),
+  },
+  tailwindcss: {
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
+    configPath: 'tailwind.config',
+    exposeConfig: {
+      level: 2
+    },
+    config: {},
+    viewer: true,
+  }
 })
