86f20abb16bf1e0147f7f41f27e6f0bfb811e05c
pdmuds4 <ryosuke3191@gmail.com>
2024-09-27 14:49:42 +0900

    /couponsページとListItemコンポーネントを実装

---
+++ frontend/pages/coupons.vue
@@ -0,0 +1,79 @@
+<template>
+    <ProviderDrawer>
+        <div class="coupons-content">
+            <Heading class="coupons-title">クーポン一覧</Heading>
+            <div class="coupons-filter">
+                <Selector 
+                    class="coupons-filter-selector"
+                    bgImage="img/components/Selector/selector-filter.svg"
+                    @change-handler="(e:Event)=>console.log(e.target)"
+                >
+                    <option value="" selected>なし</option>
+                    <option value="1">期間中</option>
+                    <option value="2">期間終了</option>
+                    <option value="3">使用済み</option>
+                </Selector>
+                <Selector 
+                    class="coupons-filter-selector"
+                    bgImage="img/components/Selector/selector-list.svg"
+                    @change-handler="(e:Event)=>console.log(e.target)"
+                >
+                    <option value="" selected>期限順</option>
+                    <option value="1">登録順</option>
+                </Selector>
+            </div>
+            <div class="coupons-list">
+                <UiCouponListItem />
+                <UiCouponListItem />
+                <UiCouponListItem />
+            </div>
+        </div>
+    </ProviderDrawer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.coupons-title {
+    font-size: 28px;
+}
+
+.coupons-content {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 10px;
+}
+
+.coupons-list {
+    display: flex;
+    flex-direction: column;
+    gap: 4vh;
+}
+
+.coupons-filter {
+    width: 50%;
+    display: flex;
+    gap: 10px;
+    margin-left: auto;
+}
+
+.coupons-filter-selector {
+    padding: 2px 10px;
+    width: 50%;
+}
+
+@media screen and (max-width: 580px) {
+    .coupons-content {
+        padding: 0px;
+    }
+
+    .coupons-filter {
+        width: 80%;
+        margin-left: auto;
+    }
+}
+
+</style>
