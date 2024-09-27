3288df106efa960975b4087661285304b9be83fb
pdmuds4 <ryosuke3191@gmail.com>
2024-09-27 14:46:33 +0900

    Accordionの点線部分をタブ側に移動

--- frontend/components/Accordion.vue
+++ frontend/components/Accordion.vue
@@ -22,9 +22,7 @@
 <template>
     <div class="accordion-body" :style="{flexDirection: reverse ? 'row-reverse' : undefined}">
         <!-- 内部コンテンツ -->
-        <div class="accordion-contents" 
-            :style="reverse ? {borderLeft: '4px dashed #34495E'} : {borderRight: '4px dashed #34495E'}"
-        >
+        <div class="accordion-contents">
             <div class="accordion-show">
                 <slot name="show-contents">
                     <h1>Show-Contents</h1>
@@ -38,7 +36,11 @@
         </div>
 
         <!-- 開閉ボタン部分 -->
-        <div class="accordion-tab" @click="openAccordion">
+        <div 
+            class="accordion-tab" 
+            @click="openAccordion"
+            :style="reverse ? {borderRight: '4px dashed #34495E'} : {borderLeft: '4px dashed #34495E'}"
+        >
             <NuxtImg 
                 class="accordion-btn"
                 :src="`img/components/Accordion/${ is_open ? 'close-btn.svg' : 'open-btn.svg'}`"
@@ -69,23 +71,23 @@
     }
 
     .accordion-contents {
+        width: 100%;
         padding: 10px 20px;
-        width: 88%;
         display: flex;
         flex-direction: column;
         gap: 15px;
     }
 
     .accordion-tab {
-        width: 15%;
+        width: 50px;
         display: flex;
         cursor: pointer;
         justify-content: center;
+        padding: 10px 0;
     }
 
     .accordion-btn {
-        height: auto;
-        aspect-ratio: 1/1;
+        height: 30px;
         width: 30px;
     }
 
@@ -93,7 +95,6 @@
     .accordion-contents {
         padding: 10px;
     }
-
 }
 
 </style>
