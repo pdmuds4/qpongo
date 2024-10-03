5cf8414a6b3ada2564d2670dc8ade20f6854f051
pdmuds4 <ryosuke3191@gmail.com>
2024-10-03 14:10:00 +0900

    UserのValueObjectを実装

--- frontend/models/value_object/id.ts
+++ frontend/models/value_object/id.ts
@@ -2,7 +2,7 @@ import AbsValueObject from "~/models/_abstruct/value_object";
 import ValueObjectError from "./error";
 
 export default class Id extends AbsValueObject<number> {
-    validate(const_val: number): number {
+    validate(const_val: number) {
         if (const_val <= 0) throw new ValueObjectError('IDは0以下ではいけません');
         
         return const_val;
---
+++ frontend/models/value_object/user/name.ts
@@ -0,0 +1,11 @@
+import AbsValueObject from "~/models/_abstruct/value_object";
+import ValueObjectError from "../error";
+
+export default class UserName extends AbsValueObject<string> {
+    validate(const_val: string) {
+        if (const_val.length < 1) throw new ValueObjectError('ユーザ名は1文字以上である必要があります');
+        if (const_val.length > 8) throw new ValueObjectError('ユーザ名は8文字以下である必要があります');
+
+        return const_val;
+    }
+}
