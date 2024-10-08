9d6125afc9dffcb5118b9a9de93b21b0fcbfceed
pdmuds4 <ryosuke3191@gmail.com>
2024-10-09 02:31:11 +0900

    get_user_settingsのdtoを実装

--- frontend/models/dto/settings/req_get_user_settings.ts
+++ frontend/models/dto/settings/req_get_user_settings.ts
@@ -1,13 +1,22 @@
 import AbsDTO from "~/models/_abstruct/dto";
+import type Id from "~/models/value_object/id";
+import type SettingNotice from "~/models/value_object/setting/notice";
 
 export type GetUserSettingsReqJson = {
-    none: null
+    user_id: number;
 }
 
 export default class GetUserSettingsReqDTO extends AbsDTO<GetUserSettingsReqJson> {
+    user_id: Id;
+
+    constructor(user_id: Id, notice: SettingNotice) {
+        super();
+        this.user_id = user_id;
+    }
+
     toJson(): GetUserSettingsReqJson {
         return {
-            none: null
+            user_id: this.user_id.value
         }
     }
 }
