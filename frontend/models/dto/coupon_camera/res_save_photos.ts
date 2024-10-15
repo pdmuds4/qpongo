a2a1f4cac374a316306a905bf4891c210c0cb3c8
pdmuds4 <ryosuke3191@gmail.com>
2024-10-15 11:40:25 +0900

    型がEntitiyになってたのを修正

--- frontend/models/dto/coupon_camera/res_save_photos.ts
+++ frontend/models/dto/coupon_camera/res_save_photos.ts
@@ -2,8 +2,8 @@ import AbsDTO from "~/models/_abstruct/dto";
 import type CouponPhoto from "~/models/value_object/coupon/photo";
 
 export type SavePhotosResJson = {
-    photo_front: CouponPhoto;
-    photo_back: CouponPhoto;
+    photo_front: string;
+    photo_back: string;
 }
 
 export default class SavePhotosResDTO extends AbsDTO<SavePhotosResJson> {
@@ -21,8 +21,8 @@ export default class SavePhotosResDTO extends AbsDTO<SavePhotosResJson> {
 
     toJson(): SavePhotosResJson {
         return {
-            photo_front: this.photo_front,
-            photo_back: this.photo_back
+            photo_front: this.photo_front.value,
+            photo_back: this.photo_back.value
         }
     }
 }
