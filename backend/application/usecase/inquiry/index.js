f8be3c4bb90f480572245733eb5ed18a2435672b
Kanra_Ishido <s2222002@stu.musashino-u.ac.jp>
2024-10-16 18:20:22 +0900

    お問い合わせのユースケースを変更

--- backend/application/usecase/inquiry/index.js
+++ backend/application/usecase/inquiry/index.js
@@ -1,11 +1,5 @@
-const AddInquiryUseCase = require('./add_inquiry');
-const GetInquiryUseCase = require('./get_inquiry');
-const UpdateInquiryUseCase = require('./update_inquiry');
-const DeleteInquiryUseCase = require('./delete_inquiry');
+const InquiryUseCase = require('./inquiry');
 
 module.exports = {
-    AddInquiryUseCase,
-    GetInquiryUseCase,
-    UpdateInquiryUseCase,
-    DeleteInquiryUseCase
+    InquiryUseCase
 }
