d0392aaca0ed5f75087ef4ada5555e18040ca8d5
Kanra_Ishido <s2222002@stu.musashino-u.ac.jp>
2024-10-17 20:13:36 +0900

    クーポンの編集するルートを作成

--- backend/application/usecase/coupon/coupon_edit.js
+++ backend/application/usecase/coupon/coupon_edit.js
@@ -9,19 +9,21 @@ class CouponEditUseCase {
         this.request = request;
     }
 
-    async excute() {
+    async execute() {
+        const japan_time = new Date().toLocaleString("ja-JP", {timeZone: "Asia/Tokyo"});
+        const japan_deadline = new Date(this.request.deadline).toLocaleString("ja-JP", {timeZone: "Asia/Tokyo"});
         const coupon = new CouponEntity(
             new IDValueObject(this.request.id),
             new IDValueObject(this.request.user_id),
             new GoodsValueObject(this.request.goods),
             new DiscountValueObject(this.request.discount),
             new StoreValueObject(this.request.store),
-            new DeadlineValueObject(new Date(this.request.deadline)),
+            new DeadlineValueObject(new Date(japan_deadline)),
             new PhotoValueObject(this.request.photo_front),
             new PhotoValueObject(this.request.photo_back),
             new IsUseValueObject(this.request.is_use),
             new CategoryValueObject(this.request.category),
-            new CreateDateValueObject(new Date())
+            new CreateDateValueObject(new Date(japan_time))
         );
 
         const response = await this.repository.updateCouponData(coupon);
--- backend/routes/coupon.js
+++ backend/routes/coupon.js
@@ -33,6 +33,27 @@ coupon_route.post('/', async(req, res) => {
     return res.send(coupon_registered);
 });
 
+coupon_route.put('/', async(req, res) => {
+    const repository = new CouponRepository(dynamoDBDocumentClient);
+
+    const coupon = {
+        id: req.body.id,
+        user_id: req.body.user_id,
+        goods: req.body.goods,
+        discount: req.body.discount,
+        store: req.body.store,
+        deadline: req.body.deadline,
+        photo_front: req.body.photo_front,
+        photo_back: req.body.photo_back,
+        is_use: req.body.is_use,
+        category: req.body.category
+    }
+
+    const coupon_edit = new CouponEditUseCase(repository, coupon);
+    const coupon_edited = await coupon_edit.execute();
+    return res.send(coupon_edited);
+});
+
 photos_route.post('/', async(req, res) => {
     const client = GPTAPIClient
     const photos_url = req.body;
