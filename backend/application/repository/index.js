a136ae4739e26cf21cccc9c734d541558c2c0997
Kanra_Ishido <s2222002@stu.musashino-u.ac.jp>
2024-10-17 16:00:17 +0900

    リポジトリクラスのルートをindexに集約

---
+++ backend/application/repository/index.js
@@ -0,0 +1,11 @@
+const UserRepository = require('./user');
+const CouponRepository = require('./coupon');
+const SettingRepository = require('./setting');
+const InquiryRepository = require('./inquiry');
+
+module.exports = {
+    UserRepository,
+    CouponRepository,
+    SettingRepository,
+    InquiryRepository
+}
