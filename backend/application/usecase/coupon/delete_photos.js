345de2b564d80d32303fd1240265b44de258edcd
Kanra_Ishido <s2222002@stu.musashino-u.ac.jp>
2024-10-17 18:45:25 +0900

    s3の写真を削除するユースケースを修正

--- backend/application/usecase/coupon/delete_photos.js
+++ backend/application/usecase/coupon/delete_photos.js
@@ -1,4 +1,5 @@
 const {PhotoValueObject} = require('../../../domain/value_object/coupon');
+const {DeleteObjectCommand} = require('@aws-sdk/client-s3');
 require('dotenv').config();
 
 class CouponPhotosDeleteUseCase {
@@ -11,21 +12,22 @@ class CouponPhotosDeleteUseCase {
         const bucketName = process.env.AWS_S3_BUCKET_NAME;
         const region = process.env.AWS_REGION;
 
-        for (const photo_key in this.request) {
-            const url = new PhotoValueObject(this.request[photo_key]);
-            const key = url.value.replace(`https://${bucketName}.s3.${region}.amazonaws.com/`, '');
-            const params = {
-                Bucket: bucketName,
-                Key: key
-            };
+        try{
+            for (const photo_key in this.request) {
+                const url = new PhotoValueObject(this.request[photo_key]);
+                const key = url.value.replace(`https://${bucketName}.s3.${region}.amazonaws.com/`, '');
+                const params = {
+                    Bucket: bucketName,
+                    Key: key
+                };
 
-            try {
                 const command = new DeleteObjectCommand(params);
                 await this.client.send(command);
-                return '写真を削除しました。'
-            } catch (error) {
-                throw new Error(error);
             }
+            const return_json = {message: '写真を削除しました。'};
+            return return_json;
+        } catch (error) {
+            throw new Error(error);
         }
     }
 }
