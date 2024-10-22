7d1b864b87a5dd17b2bc6c82ba30c4d0e5b62499
Kanra_Ishido <s2222002@stu.musashino-u.ac.jp>
2024-10-22 17:07:36 +0900

    hedersにAPI KeyがないとAPIにアクセスできないようなミドルウェアを設置

--- backend/main.js
+++ backend/main.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
@@ -5,10 +7,25 @@ const port = 3000
 
 const {user_route, coupon_route, photos_route, setting_route, inquiry_route} = require('./routes')
 
+const middleware = (req, res, next) => {
+    const apiKey = req.headers['x-api-key']
+
+    if (apiKey) {
+        if (apiKey === process.env.API_KEY_CERTIFICAYION) {
+            next()
+        } else {
+            res.status(403).json({message: 'API Key is invalid'})
+        }
+    } else {
+        res.status(401).json({message: 'API Key is required'})
+    }
+}
+
+app.use(middleware)
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send("Hello World!!!")
+    res.send("Hello World!!!")
 })
 
 app.use('/user', user_route)
