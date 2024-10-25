b51936bb97e53ea4ce1b1d4869825319b2d8a226
pdmuds4 <ryosuke3191@gmail.com>
2024-10-25 10:45:24 +0900

    debugモードをfalse

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     }
   },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/image',
