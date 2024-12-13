8c089fe62431843d32bf4b5b66bf8a4b1c6b0820
pdmuds4 <ryosuke3191@gmail.com>
2024-12-13 14:00:11 +0900

    必要ソースを用意した

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-    <div>
+    <ProviderLandingContent header-btn="login">
+        <section class="lp-section first">
+            <div class="section1-text">
+                <h1>おトクをもっと</h1>
+                <h1>あなたのそばに</h1>
+                <p>紙クーポン管理アプリ</p>
+            </div>
+            <div class="section1-img">
+                <NuxtImg class="section1-symbol" src="img/logo.svg" alt="symbol" />
+            </div>
+        </section>
+        <section class="lp-section second">
 
-    </div>
+        </section>
+        <section class="lp-section third">
+
+        </section>
+    </ProviderLandingContent>
 </template>
 
 <script setup lang="ts">
@@ -9,5 +24,14 @@
 </script>
 
 <style scoped>
+.first {
+    display: flex;
+    gap: 10px;
+    width: 100%;
+}
+
+.section1-text .section1-img {
+    width: 50%;
+}
 
 </style>
